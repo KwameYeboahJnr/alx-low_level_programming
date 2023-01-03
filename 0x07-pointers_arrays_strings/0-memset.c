ab2ad630521c5ec4d7eec34c6aa1dcac6fc13098
KwameYeboahJnr <kwayebs.jnr@gmail.com>
2023-01-03 11:56:28 -0800

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,17 +2,18 @@
 /**
  *_memset - fills the memory with a constant byte
  *@s: pointer to the memory area to be filled
- *@c: character to fill the memory area
+ *@b: desired value
  *@n: the number of bytes to be filled
- *description _memset: over ther
- *Return: a pointer to the filled memory area @s
+ *Return: changed array with new value for n bytes
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	int i = 0;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-	return (memory);
+	for (; n < 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
